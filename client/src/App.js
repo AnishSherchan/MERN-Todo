f28083cdfb6b0123241201b9e1db69ed1a74fe1d
AnishSherchan <sherchananish11@gmail.com>
2023-08-25 01:00:20 +0545

    Testing for routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Landing from "./Pages/landing";
+import { Link } from "react-router-dom";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Landing />,
+    },
+    {
+      path: "/test",
+      element: (
+        <div>
+          hi <Link to="/">landing</Link>
+        </div>
+      ),
     },
   ]);
 
