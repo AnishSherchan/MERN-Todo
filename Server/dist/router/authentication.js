5f4d9573da88dc6df781b697efc563b6f8697984
AnishSherchan <sherchananish11@gmail.com>
2023-08-26 23:21:09 +0545

    Removed dist

--- Server/dist/router/authentication.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const authentication_1 = require("../controller/authentication");
-exports.default = (router) => {
-    router.post("/auth/register", authentication_1.registerUser);
-    router.post("/auth/login", authentication_1.userLogin);
-};
-//# sourceMappingURL=authentication.js.map
