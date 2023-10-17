12da2bd7bce8c9e5ae29d3348e3ea8d52c58d9da
unknown <sherchananish11@gmail.com>
2023-10-18 00:44:19 +0545

    User Auth Bug Fixed

--- Server/src/controller/authentication.ts
+++ Server/src/controller/authentication.ts
@@ -11,12 +11,10 @@ export const registerUser = async (
     if (!email || !password || !username) {
       return res.sendStatus(400);
     }
-
     const existingUser = await getUserByEmail(email);
     if (existingUser) {
       return res.sendStatus(400);
     }
-
     const salt = random();
     const user = await createUser({
       email,
@@ -26,6 +24,7 @@ export const registerUser = async (
         password: authentication(salt, password),
       },
     });
+    return res.status(200).json(user).end();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
