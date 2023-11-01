89d80c15f685378cfc1ed8ad5957dd1c5b24e3a1
unknown <sherchananish11@gmail.com>
2023-11-01 12:27:21 +0545

    credentials

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -9,26 +9,7 @@ import router from "./router";
 require("dotenv").config();
 
 const app = express();
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://mern-todo-hazel.vercel.app",
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true, // enable set cookie
-  })
-);
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(compression());
