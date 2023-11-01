b06a90781dc54567a3713d23949046547d62af8b
unknown <sherchananish11@gmail.com>
2023-11-01 12:22:49 +0545

    credentials: true, // enable set cookie New code

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -9,9 +9,22 @@ import router from "./router";
 require("dotenv").config();
 
 const app = express();
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://mern-todo-hazel.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "*",
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true, // enable set cookie
   })
 );
